a36fc82a9389a5b483d1d61b666cc7e58cb6d71e
Danfer Marcelo Ore <danfer.ore.martinez@gmail.com>
2026-06-02 21:41:31 -0500

    Create 01_exploracion_bd.sql

---
+++ 2_Analisis-de-Datos-SQL-SERVER/scripts/01_exploracion_bd.sql
@@ -0,0 +1,28 @@
+/*
+=============================================
+EXPLORANDO LAS TABLAS Y LAS COLUMNAS DE LA BD
+=============================================
+
+PROPOSITO DEL SCRIPT:
+	Analizar las diversas tablas y columnas de la BD, con el propósito de tener una referencia de los datos con los 
+	que vamos a trabajar 
+*/
+
+USE DataWarehouseAnalytics;
+
+-- Explorando las tablas 
+SELECT 
+    TABLE_CATALOG, 
+    TABLE_SCHEMA, 
+    TABLE_NAME, 
+    TABLE_TYPE
+FROM INFORMATION_SCHEMA.TABLES;
+
+-- Explorando las columnas 
+SELECT 
+    COLUMN_NAME, 
+    DATA_TYPE, 
+    IS_NULLABLE, 
+    CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'oro.dim_clientes';
